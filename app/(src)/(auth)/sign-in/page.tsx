8bc5c42457ea346af852d3220fa72ef0a7b94439
RamonsArchive <rmcdeem.m@gmail.com>
2025-03-26 14:39:36 -0700

    Changed placmeent of suspense and used ppr

--- app/(src)/(auth)/sign-in/page.tsx
+++ app/(src)/(auth)/sign-in/page.tsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 import Link from "next/link";
 import SignInContent from "@/components/SignInContent";
 
-const SignInPage = () => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const experimental_ppr = true;
 
+const SignInPage = () => {
   return (
+    <Suspense fallback={<div>Loading... </div>}>
     <main className="flex fixed top-0 left-0 w-full h-full justify-center items-center bg-white-300 overflow-y-hidden">
       <div className="flex flex-col max-w-[400px] max-h-[550px] w-[80%] h-[80%] font-plex-sans border border-gray-300 shadow-lg items-center gap-10 ">
         <div className="flex flex-col gap-5 py-[25%] w-full items-center">
@@ -18,12 +21,13 @@ const SignInPage = () => {
             />
           </Link>
 
-          <Suspense fallback={<div>Loading... </div>}>
+          
           <SignInContent />
-          </Suspense>
+          
         </div>
       </div>
     </main>
+    </Suspense>
   );
 };
 
