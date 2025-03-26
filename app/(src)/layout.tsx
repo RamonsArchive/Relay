a8ec7bafe5443a8d3927ca396c5c313d0efada8b
RamonsArchive <rmcdeem.m@gmail.com>
2025-03-26 15:49:37 -0700

    Added suspense to layout components

--- app/(src)/layout.tsx
+++ app/(src)/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import { ContextProvider } from "@/app/context/context";
-import React from "react";
+import React, { Suspense } from "react";
 import SidebarWrapper from "@/components/SidebarWrapper";
 import { Toaster } from "sonner";
 
@@ -9,16 +9,22 @@ const layout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <ContextProvider>
+        <Suspense fallback={<div>Loading...</div>}>
         <Navbar />
+        </Suspense>
         <div className="root-container">
+          <Suspense fallback={<div>Loading...</div>}>
           <SidebarWrapper />
+          </Suspense>
 
           <div className="content-wrapper">
             <div className="content-container">{children}</div>
           </div>
         </div>
         <div className="footer-wrapper">
+          <Suspense fallback={<div>Loading...</div>}>
           <Footer />
+          </Suspense>
         </div>
         <Toaster richColors />
       </ContextProvider>
